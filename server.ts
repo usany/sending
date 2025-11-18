18967f135c05611c194a8015c28920c59202f47b
usany <ckd_qja@naver.com>
2025-11-19 03:42:11 +0900

    cern

--- server.ts
+++ server.ts
@@ -44,30 +44,6 @@ const createTransporter = async () => {
 
   return transporter;
 };
-await new Promise((resolve, reject) => {
-    // verify connection configuration
-    transporter.verify(function (error, success) {
-        if (error) {
-            console.log(error);
-            reject(error);
-        } else {
-            console.log("Server is ready to take our messages");
-            resolve(success);
-        }
-    });
-});
-await new Promise((resolve, reject) => {
-    // send mail
-    transporter.sendMail(mailData, (err, info) => {
-        if (err) {
-            console.error(err);
-            reject(err);
-        } else {
-            console.log(info);
-            resolve(info);
-        }
-    });
-});
 
 const sendEmail = async (emailOptions: MailOptions) => {
   try {
