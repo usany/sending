732931a6de48956149d675e9074c2af1ae842c0f
usany <ckd_qja@naver.com>
2025-12-21 01:11:29 +0900

    cern

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
-import cors from 'cors';
-import express from 'express';
-import { google } from 'googleapis';
-import { createServer } from 'http';
-import nodemailer from 'nodemailer';
+import cors from 'cors'
+import express from 'express'
+import { google } from 'googleapis'
+import { createServer } from 'http'
+import nodemailer from 'nodemailer'
 
 // import dotenv from 'dotenv'
 // import { type MailOptions } from 'nodemailer/lib/sendmail-transport/index.js'
@@ -10,17 +10,17 @@ import nodemailer from 'nodemailer';
 
 // dotenv.config()
 const createTransporter = async () => {
-	const OAuth2 = google.auth.OAuth2;
+	const OAuth2 = google.auth.OAuth2
 	const oauth2Client = new OAuth2(
 		process.env.CLIENTID,
 		process.env.CLIENTSECRET,
 		'https://developers.google.com/oauthplayground',
-	);
+	)
 	oauth2Client.setCredentials({
 		refresh_token: process.env.REFRESHTOKEN,
-	});
+	})
 
-	const { token: accessToken } = await oauth2Client.getAccessToken();
+	const { token: accessToken } = await oauth2Client.getAccessToken()
 
 	const transporter = nodemailer.createTransport({
 		service: 'gmail',
@@ -32,23 +32,23 @@ const createTransporter = async () => {
 			clientSecret: process.env.CLIENTSECRET,
 			refreshToken: process.env.REFRESHTOKEN,
 		},
-	});
+	})
 
-	return transporter;
-};
+	return transporter
+}
 const sendEmail = async (emailOptions: nodemailer.SendMailOptions) => {
 	try {
-		const emailTransporter = await createTransporter();
-		const res = await emailTransporter.sendMail(emailOptions);
-		console.log('sending');
-		return res;
+		const emailTransporter = await createTransporter()
+		const res = await emailTransporter.sendMail(emailOptions)
+		console.log('sending')
+		return res
 	} catch (error) {
-		console.log(error);
+		console.log(error)
 	}
-};
+}
 
-const app = express();
-const httpServer = createServer(app);
+const app = express()
+const httpServer = createServer(app)
 const corsOptions = {
 	// origin: '*',
 	// origin: 'http://localhost:5173',
@@ -62,39 +62,39 @@ const corsOptions = {
 		'https://khusan.co.kr',
 	],
 	optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-app.use(express.json());
+}
+app.use(cors(corsOptions))
+app.use(express.json())
 app.post('/mail', async (req, res) => {
 	try {
-		const reqMethod = req.method;
-		const reqURL = req.url;
-		console.log(`${reqMethod} ${reqURL}`);
-		const language = req.body.language;
+		const reqMethod = req.method
+		const reqURL = req.url
+		console.log(`${reqMethod} ${reqURL}`)
+		const language = req.body.language
 		const subject =
 			language === 'ko'
 				? '환영합니다 쿠우산입니다! 가입 번호입니다.'
-				: 'Welcome to KHUSAN! Here is the verification number.';
+				: 'Welcome to KHUSAN! Here is the verification number.'
 		const text =
 			language === 'ko'
 				? `환영합니다. 번호는 ${req.body.number}입니다. 문의사항은 메일로 보내주세요.`
-				: `Welcome. The number is ${req.body.number}. Kindly send any inquiries to this email.`;
+				: `Welcome. The number is ${req.body.number}. Kindly send any inquiries to this email.`
 		if (reqMethod === 'POST' && reqURL === '/mail') {
-			console.log('sending');
+			console.log('sending')
 			await sendEmail({
 				subject: subject,
 				text: text,
 				to: req.body.to,
 				from: process.env.USER,
-			});
+			})
 		}
-		res.send(JSON.stringify({ res: 'sending' }));
+		res.send(JSON.stringify({ res: 'sending' }))
 	} catch (error) {
-		console.log(error);
-		res.send(JSON.stringify({ res: 'error' }));
+		console.log(error)
+		res.send(JSON.stringify({ res: 'error' }))
 	}
-});
+})
 
 httpServer.listen(5000, () => {
-	console.log('ready');
-});
+	console.log('ready')
+})
